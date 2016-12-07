c10d9d8e4a2e136e0020099dc1978521e8b1ccf9
Jeroen Stevens <jeroenstevens@gmail.com>
2016-12-06 21:17:24 -0800

    Update carcontroller.php

--- app/controllers/carcontroller.php
+++ app/controllers/carcontroller.php
@@ -39,35 +39,33 @@ class CarController extends BaseController{
 
     public function retrieve(){	
 	if($this->validCategory){
-		if($this->route['id'] == ""){
-			$data = $this->dataRepository->getProducts();
-		}else if(isset($this->route['id']) && $this->route['id'] != ""){
-			$data = $this->dataRepository->getProduct($this->route['id']);
-		}
-		$this->responseHandler->handler(new Response($data,null));
+	    if($this->route['id'] == ""){
+		$data = $this->dataRepository->getProducts();
+	    }else if(isset($this->route['id']) && $this->route['id'] != ""){
+		$data = $this->dataRepository->getProduct($this->route['id']);
+	    }
+	    $this->responseHandler->handler(new Response($data,null));
 	}else{
-		$this->responseHandler->handler(new Response(null, Errors::handleError('')));
+	    $this->responseHandler->handler(new Response(null, Errors::handleError('')));
         }
     }
 
     public function process(){
-	if($this->validCategory){
-			
-    		$dataComplete = DataHelper::sanitize($this->dataKeys,$sanitizedPost);
+	if($this->validCategory){	
+    	    $dataComplete = DataHelper::sanitize($this->dataKeys,$sanitizedPost);
 		
-		if($dataComplete){
-			$result = $this->dataRepository->setProduct($sanitizedPost);
-                	if($result == true){
-	    			$this->responseHandler->handler(new Response(["POST"=>"Succes","Data"=>$sanitizedPost],null));
-			}
-	    		$this->responseHandler->handler(new Response(null,Errors::handleError('eMsgPostDataIncomplete')));             
-			}else{
-				$this->responseHandler->handler(new Response(null,Errors::handleError('eMsgPostDataIncomplete')));
-			}
-		}else{
-		    $this->responseHandler->handler(new Response(null, Errors::handleError('')));
+	    if($dataComplete){
+		$result = $this->dataRepository->setProduct($sanitizedPost);
+		if($result == true){
+		    $this->responseHandler->handler(new Response(["POST"=>"Succes","Data"=>$sanitizedPost],null));
 		}
-
+		$this->responseHandler->handler(new Response(null,Errors::handleError('eMsgPostDataIncomplete')));             
+	    }else{
+	        $this->responseHandler->handler(new Response(null,Errors::handleError('eMsgPostDataIncomplete')));
+	    }
+        }else{
+	    $this->responseHandler->handler(new Response(null, Errors::handleError('')));
+        }
     }
 
 }
